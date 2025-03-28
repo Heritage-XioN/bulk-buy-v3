ce5e44e87994f0aa6c4bee379355e09a77964812
Ikechi Emmanuel <ikechiemmanuelice@gmail.com>
2025-03-28 08:59:21 +0100

    Fixing merge issue

---
+++ src/app/auth/login/page.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import Form from "@/components/molecules/loginForm/Form.jsx"
+import "./login.css"
+import Link from 'next/link'
+import Image from "next/image";
+
+
+export default function Login() {
+  return (
+    <div className="container">
+        <div className="sec-two">
+        <Image src='/images/bulk-buy-people.jpg' fill={true} style={{objectFit: "cover"}} alt="sideframe"/>
+        </div>
+        <div className="sec-one">
+            <h1>Login</h1>
+            <h4>Welcome back to the smart shopping platform</h4>
+            <Form  />
+
+            <p> 
+            Dunt have an acount? 
+            <Link href='/auth/signup' className='link'>
+              Signup
+            </Link>
+            </p>
+        </div>
+    </div>
+  )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
